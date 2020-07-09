ebcecc4705ef9daec6436262a33dcb7d6069c39e
Ziqi Zhou <ziqizhou@Ziqis-MacBook-Pro.local>
2020-07-09 19:14:11 -0400

    change

--- covid.R
+++ covid.R
@@ -1,3 +1,4 @@
+##load packages
 library(sp)
 library(tmap)  
 library(tidyverse)
@@ -7,50 +8,37 @@ library(leaflet)
 library(tigris)
 library(plotly)
 library(shinyWidgets)
-
 library(shiny)
 
-#get data
+##get data
 spdf = rgdal::readOGR("Geography-resources/MODZCTA_2010_WGS1984.geo.json")
 spdf@data
 #June data
 Junedata = read.csv("./data/Junedata.csv")
-
-
 #input the widges
 plot_date = Junedata %>% distinct(day) %>% pull()
+outcome_selection = c("positive","covid_death_count","covid_case_rate","covid_death_rate","newcases_june")
 
 
-#join the data
-Map_data = geo_join(spdf,Junedata,"MODZCTA","zipcode")
-
-# Setting up the pop up text
-popup_sb <- paste0("Positive Cases: ", as.character(Map_data$positive))
-
-pal <- colorNumeric("Greens", domain=Map_data$positive)
 
 
-popup_dea_sb <- paste0("Cumulative Cases Count", as.character(Map_data$positive),
-                       "<br>",
-                       "Death Count: ", as.character(Map_data$covid_death_count),
-                       "<br>", 
-                       "Neighborhood Name: ", as.character(Map_data$neighborhood_name),
-                       "MODZCTA: ", as.character(Map_data$MODZCTA))
-
-pal_dea <- colorNumeric("Reds", domain=Map_data$covid_death_count)
-
-
-###Map functions###
-
-# function to plot cumulative COVID cases by date
-cumulative_plot = function(outcome, plot_date) {
-  data_to_plot = Map_data %>%
-    filter(day == input$date_choice) %>% 
-
+  Map_data = geo_join(spdf,plot_df,"MODZCTA","zipcode")
+  
+  popup_sb <- paste0("Neighborhood Name: ", as.character(Map_data$neighborhood_name),
+                     "<br>", 
+                     "MODZCTA: ", as.character(Map_data$MODZCTA),
+                     "<br>", 
+                     "Total Number of ",outcome," Cases", as.character(Map_data$outcome)
+  )
+  
+  # Setting up the pop up text
+  pal <- colorNumeric("Reds", domain=Map_data$outcome)
+  
+  
   g1 = leaflet() %>%
     addProviderTiles("CartoDB.Positron") %>%
     setView(lng = -73.99653, lat = 40.75074, zoom = 10) %>% 
-    addPolygons(data = data_to_plot , 
+    addPolygons(data = Map_data , 
                 fillColor = ~pal(Map_data$outcome), 
                 fillOpacity = 0.7, 
                 weight = 0.2, 
@@ -60,25 +48,8 @@ cumulative_plot = function(outcome, plot_date) {
               values = Map_data$outcome, 
               position = "bottomright", 
               title = "Number")
-
   g1
-}
 
-
-
-
-# function to plot new COVID cases by date
-new_cases_plot = function(cv_aggregated, plot_date) {
-  plot_df_new = subset(cv_aggregated, date<=plot_date)
-  g1 = ggplot(plot_df_new, aes(x = date, y = new, fill = region)) + 
-    geom_bar(position="stack", stat="identity") + 
-    ylab("new cases") + theme_bw() + 
-    scale_fill_manual(values=c(covid_col)) +
-    scale_y_continuous(labels = function(l) {trans = l / 1000; paste0(trans, "K")}) +
-    theme(legend.title = element_blank(), legend.position = "", plot.title = element_text(size=10), 
-          plot.margin = margin(5, 12, 5, 5))
-  g1
-}
 
 
 ##########
@@ -93,7 +64,7 @@ ui =  fluidPage(
                   label = h3("Select date"),
                   choices = plot_date),
       
-      pickerInput("outcome_select", "Outcome:",   
+      pickerInput(inputId = "outcome",label =  "Outcome:",   
                   choices = c("Cumulative Cases Count", "Death Count", "Positive Cases Rate", "Death Rate","New cases"), 
                   selected = c("Cumulative Cases Count"),
                   multiple = FALSE)
@@ -111,12 +82,12 @@ ui =  fluidPage(
 
 
 server = function(input, output,session) {
-
-    output$cumulative_plot <- renderPlot({
-      cumulative_plot(input$outcome, input$plot_date)
-    })
+  
+  
+  output$map1 = renderLeaflet({
     
     
+  })
 }
 
 
---
+++ covid_map_try.R
@@ -0,0 +1,225 @@
+library(sp)
+library(tmap)  
+library(tidyverse)
+library(readxl)
+library(viridis)
+library(leaflet)
+library(tigris)
+library(plotly)
+library(shinyWidgets)
+library(shiny)
+
+#get data
+spdf = rgdal::readOGR("Geography-resources/MODZCTA_2010_WGS1984.geo.json")
+spdf@data
+
+Junedata = read.csv("./data/Junedata.csv")
+#input the widges
+date = Junedata %>% distinct(day) %>% pull()
+
+
+
+
+
+##########
+
+### ui
+ui =  fluidPage(
+  titlePanel("COVID-19 in NYC"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create maps for the distribution of Cumulative Positive cases in NYC."),
+      
+      selectInput(
+        "date_choice", 
+        label = h3("Select date"),
+        choices = date),
+      
+      radioButtons(inputId = "outcome_selection",
+                  label =  "Outcome:",   
+                  choices = c("Cumulative Cases Count", "Death Count", "Positive Cases Rate", "Death Rate","New cases"), 
+                  selected = c("Cumulative Cases Count")
+                 
+    )),
+    
+    
+    mainPanel(leafletOutput(outputId = "map",width="100%",height="600px"))
+    
+  ))
+
+
+###### server
+
+server = function(input, output) {
+
+
+    
+  
+  output$map = renderLeaflet({
+    data_to_plot = Junedata %>% filter(day == input$date_choice)
+    data_to_plot_geo = geo_join(spdf,data_to_plot,"MODZCTA","zipcode")
+ 
+    if (input$outcome_selection == "New cases"){
+    pal <- colorNumeric("Greens", domain=Junedata$newcases_june)
+      
+    popup_sb <- paste0("Neighborhood Name: ", as.character(Junedata$neighborhood_name),
+                         "<br>", 
+                         "MODZCTA: ", as.character(Junedata$zipcode),
+                         "<br>", 
+                         "Total Number of Cumulative Positive Cases: ", as.character(Junedata$newcases_june)
+      )
+      
+      leaflet() %>%
+        addProviderTiles("CartoDB.Positron") %>%
+        setView(lng = -73.99653, lat = 40.75074, zoom = 10) %>% 
+        addPolygons(data =  data_to_plot_geo , 
+                    fillColor = ~pal(data_to_plot_geo$newcases_june), 
+                    fillOpacity = 0.7, 
+                    weight = 0.2, 
+                    smoothFactor = 0.2, 
+                    popup = ~popup_sb) %>%
+        addLegend(pal = pal, 
+                  values =  data_to_plot_geo$newcases_june, 
+                  position = "bottomright", 
+                  title = "Number")
+      
+  }
+  
+    
+    if (input$outcome_selection=="Death Rate") { 
+    
+        data_to_plot = Junedata %>% filter(day == input$date_choice)
+        data_to_plot_geo = geo_join(spdf,data_to_plot,"MODZCTA","zipcode")
+      
+      pal <- colorNumeric("Greens", domain=Junedata$covid_death_rate)
+      
+      popup_sb <- paste0("Neighborhood Name: ", as.character(Junedata$neighborhood_name),
+                         "<br>", 
+                         "MODZCTA: ", as.character(Junedata$zipcode),
+                         "<br>", 
+                         "Total Number of Cumulative Positive Cases: ", as.character(Junedata$covid_death_rate)
+      )
+      
+      leaflet() %>%
+        addProviderTiles("CartoDB.Positron") %>%
+        setView(lng = -73.99653, lat = 40.75074, zoom = 10) %>% 
+        addPolygons(data =  data_to_plot_geo , 
+                    fillColor = ~pal(data_to_plot_geo$covid_death_rate), 
+                    fillOpacity = 0.7, 
+                    weight = 0.2, 
+                    smoothFactor = 0.2, 
+                    popup = ~popup_sb) %>%
+        addLegend(pal = pal, 
+                  values =  data_to_plot_geo$covid_death_rate, 
+                  position = "bottomright", 
+                  title = "Number")
+      
+      
+      }
+  
+    
+    if (input$outcome_selection=="Cumulative Cases Count") { 
+     
+        data_to_plot = Junedata %>% filter(day == input$date_choice)
+        data_to_plot_geo = geo_join(spdf,data_to_plot,"MODZCTA","zipcode")
+     
+      pal <- colorNumeric("Greens", domain=Junedata$positive)
+      
+      popup_sb <- paste0("Neighborhood Name: ", as.character(Junedata$neighborhood_name),
+                         "<br>", 
+                         "MODZCTA: ", as.character(Junedata$zipcode),
+                         "<br>", 
+                         "Total Number of Cumulative Positive Cases: ", as.character(Junedata$positive)
+      )
+      
+      leaflet() %>%
+        addProviderTiles("CartoDB.Positron") %>%
+        setView(lng = -73.99653, lat = 40.75074, zoom = 10) %>% 
+        addPolygons(data =  data_to_plot_geo , 
+                    fillColor = ~pal(data_to_plot_geo$positive), 
+                    fillOpacity = 0.7, 
+                    weight = 0.2, 
+                    smoothFactor = 0.2, 
+                    popup = ~popup_sb) %>%
+        addLegend(pal = pal, 
+                  values =  data_to_plot_geo$positive, 
+                  position = "bottomright", 
+                  title = "Number")
+      
+      
+      }
+      
+    if (input$outcome_selection=="Death Count") { 
+   
+        data_to_plot = Junedata %>% filter(day == input$date_choice)
+        data_to_plot_geo = geo_join(spdf,data_to_plot,"MODZCTA","zipcode")
+      
+      pal <- colorNumeric("Greens", domain=Junedata$covid_death_count)
+      
+      popup_sb <- paste0("Neighborhood Name: ", as.character(Junedata$neighborhood_name),
+                         "<br>", 
+                         "MODZCTA: ", as.character(Junedata$zipcode),
+                         "<br>", 
+                         "Total Number of Cumulative Positive Cases: ", as.character(Junedata$covid_death_count)
+      )
+      
+      leaflet() %>%
+        addProviderTiles("CartoDB.Positron") %>%
+        setView(lng = -73.99653, lat = 40.75074, zoom = 10) %>% 
+        addPolygons(data =  data_to_plot_geo , 
+                    fillColor = ~pal(data_to_plot_geo$covid_death_count), 
+                    fillOpacity = 0.7, 
+                    weight = 0.2, 
+                    smoothFactor = 0.2, 
+                    popup = ~popup_sb) %>%
+        addLegend(pal = pal, 
+                  values = data_to_plot_geo$covid_death_count, 
+                  position = "bottomright", 
+                  title = "Number")
+      
+      
+      }
+      
+    if (input$outcome_selection=="Positive Cases Rate") { 
+      
+        data_to_plot = Junedata %>% filter(day == input$date_choice)
+        data_to_plot_geo = geo_join(spdf,data_to_plot,"MODZCTA","zipcode")
+
+      pal <- colorNumeric("Greens", domain=Junedata$covid_case_rate)
+      
+      popup_sb <- paste0("Neighborhood Name: ", as.character(Junedata$neighborhood_name),
+                         "<br>", 
+                         "MODZCTA: ", as.character(Junedata$zipcode),
+                         "<br>", 
+                         "Total Number of Cumulative Positive Cases: ", as.character(Junedata$covid_case_rate)
+      )
+      
+      leaflet() %>%
+        addProviderTiles("CartoDB.Positron") %>%
+        setView(lng = -73.99653, lat = 40.75074, zoom = 10) %>% 
+        addPolygons(data =  data_to_plot_geo , 
+                    fillColor = ~pal(data_to_plot_geo$covid_case_rate), 
+                    fillOpacity = 0.7, 
+                    weight = 0.2, 
+                    smoothFactor = 0.2, 
+                    popup = ~popup_sb) %>%
+        addLegend(pal = pal, 
+                  values =  data_to_plot_geo$covid_case_rate, 
+                  position = "bottomright", 
+                  title = "Number")
+      
+    }
+    
+    
+    })
+  
+      }
+  
+    
+
+
+
+
+
+shinyApp(ui, server)
